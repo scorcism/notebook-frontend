68c9ea6b4f7f637450ea861222bce10bba289ee9
scorcism <abhishekpathak1720@gmail.com>
2022-05-28 07:31:53 +0530

    phase 1

--- src/component/About.jsx
+++ src/component/About.jsx
@@ -4,7 +4,33 @@ const About = () => {
   return (
     <>
     <div className="about">
-      <h1>Bored to make about page :)</h1>
+      <div className="about_me">
+        <ul className="ul">
+        <h2 className='fancy_heading'>Abhishek Pathak aka scorcism</h2>
+          <li className='ul_li'>I'm a computer science student.</li>
+          <li className='ul_li'>Exploring the tech which will exists tomorrow.</li>
+          <li className='ul_li'>That's it :)</li>
+          <li className='ul_li'>See yaaa</li>
+          <li className='ul_li'>:- If your are intrested in collaboration fell free to approach me -:</li>
+        </ul>
+      </div>
+      <div className="about_project">
+        <ul className="ul">
+          <h2>Tech used</h2>
+          <li className="li_head">Frontend :-</li>
+          <li className="ul_li">React JS, CSS, HTML, Google fonts, Pixabay Images</li>
+          <li className="li_head">
+          Backend :-
+          </li>
+          <li className="ul_li"> Mysql, Express, Node JS, JWT, bycrptJS, express-validator,</li>
+          <li className="li_head">Platforms: :-</li>
+          <li className="ul_li">
+            <a href="https://www.phpmyadmin.co/" target="_blank" rel="noopener noreferrer">MYSQL Hosting</a> <br/>
+            <a href="https://vercel.com/" target="_blank" rel="noopener noreferrer">App Hosting</a> <br/>
+            <a href="https://www.heroku.com/" target="_blank" rel="noopener noreferrer">DB Hosting</a> <br/>
+          </li>
+        </ul>
+      </div>
     </div>
     </>
   )
--- src/component/Footer.jsx
+++ src/component/Footer.jsx
@@ -2,13 +2,13 @@ const Footer = () => {
     return (
         <>
             <div className="footer__container">
-                <a className="no_desc   " href="">Copyright @Abhishek Pathak</a>
-                <a className="no_desc   " href="">Something</a>
+                <a className="no_desc " href="">Copyright @Abhishek Pathak</a>
+                <a className="no_desc " href="">Twitter :) </a>
                 
                 <div className="footer_right">
-                    <a className="no_desc   hover_underlinebox" href="">Frontend</a>
-                    <a className="no_desc   hover_underlinebox" href="">Backend</a>
-                    <a className="no_desc   hover_underlinebox add_bg" href="">GitHub</a>
+                    <a className="no_desc   hover_underlinebox" target="_blank" href="https://github.com/scorcism/notebook-frontend">Frontend</a>
+                    <a className="no_desc   hover_underlinebox" target="_blank" href="https://github.com/scorcism/notebook-backend">Backend</a>
+                    <a className="no_desc   hover_underlinebox add_bg" target="_blank" href="https://github.com/scorcism/">GitHub</a>
                 </div>
             </div>
         </>
